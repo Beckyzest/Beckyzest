c80ce6a4e48bdbb040eee1e2f2b3195a1810e1e8
Finding Anagram <105230545+Beckyzest@users.noreply.github.com>
2022-05-29 00:43:33 +0100

    Class and object

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-# Check if two words are anagrams 
-# Example:
-# find_anagrams("hello", "check") --> False
-# find_anagrams("below", "elbow") --> True
+class Student:    
+    def _init_(self, name, age, tracks, score):
+       self.name = name
+       self.age = age
+       self.tracks = tracks
+       self.score= score
 
 
-def find_anagram(word, anagram):
-    str_1= input("input in the first word:")
-    str_2= input("input in the second word:") 
-sort_1=sorted (str_1)
-sort_2=sorted (str_2)
-    If sort_1== sort_2 :(anagram)):
-    answer = true
-    else:
-        answer=False
-        print(answer)
-        
-        find_anagram, str2)
+    #A function to change the name of student
+    def change_name (self, new_name):
+        self.name = new_name
+        return(Bob.name)
 
-   
+    #A function to change the age of student
+    def change_age (self, age_name):
+        self.age = new_age
+        return(Bob.age)
 
+
+    #A function to change the to an existing list of tracks
+    def change_tracks (self, added_track):
+        self.tracks.append(added_tracks)
+        return(Bob.tracks)
+
+    #A function to change the age of student
+    def get_score (self):
+        return(self.score)
+
+# Here i am creating an instance of the class toether with the properties
+Bob = Student("Bob",26, ["FE", "BE"], 20.90)
+# Expected methods
+Print (Bob.change_name("Peter"))
+Print (Bob.change_age(34))
+print (Bob.add_track("UI/UX"))
+print (Bob.get_score())
