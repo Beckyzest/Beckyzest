6fae268a421e57b3a04411a23a80c78a91799ce6
Beckyzest <105230545+Beckyzest@users.noreply.github.com>
2022-05-29 00:25:38 +0100

    Finding Anagram

---
+++ main.py
@@ -0,0 +1,21 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    str_1= input("input in the first word:")
+    str_2= input("input in the second word:") 
+sort_1=sorted (str_1)
+sort_2=sorted (str_2)
+    If sort_1== sort_2 :(anagram)):
+    answer = true
+    else:
+        answer=False
+        print(answer)
+        
+        find_anagram, str2)
+
+   
+
